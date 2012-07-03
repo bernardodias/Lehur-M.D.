6a8010f36883f420b108a45333a667c125ca30e4
bernardodias <bernardo.afd@gmail.com>
2012-07-03 12:08:22 -0300

    Criacao do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LojaOnline::Application.config.secret_token = '03b88484724afe11216e247a6b00ce94ae656c7a42f5ff1afd578d79274b77646020daba513d24992b44c065dd9c5e346eb2ac53e97398a244c1fb26c676c978'
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+LojaOnline::Application.routes.draw do
+  get "welcome/index"
+  get "sessions/new"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  
+  root :to => "welcome#index"
+  
+  resources :users
+  resources :sessions
+  resources :contact
+end
